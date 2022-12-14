d4dbbe1737f452ca6c66d896b23335815a354e2f
vutuankiet4599 <vutuankiet4599@gmail.com>
2022-12-14 12:01:50 +0700

    get all api

--- src/controllers/todosController.go
+++ src/controllers/todosController.go
@@ -1,5 +1,39 @@
 package controllers
 
-func GetAllTodo() {
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type TodoList struct {
+	ID int `json:"id"`
+	Content string `json:"content"`
+}
+
+func GetAllTodo(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_list");
+	if err != nil {
+		panic(err.Error());
+	}
+	defer db.Close();
+
+	result, err := db.Query("SELECT * FROM todos");
+	if err != nil {
+		panic(err.Error());
+	}
+
+	var todo_list = make([]TodoList, 0);
+	for result.Next() {
+		var todo TodoList;
+		err = result.Scan(&todo.ID, &todo.Content);
+		if err != nil {
+			panic(err.Error());
+		}
+
+		todo_list = append(todo_list, todo);
+	}
+	json.NewEncoder(w).Encode(todo_list);
 }
--- src/routes/api.go
+++ src/routes/api.go
@@ -1,4 +1,15 @@
 package routes
 
+import (
+	"TodoList/src/controllers"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func InitAPIRoutes() {
+	r := mux.NewRouter();
+	r.HandleFunc("/api/todo", controllers.GetAllTodo);
+
+	http.Handle("/", r);
 }
